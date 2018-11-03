653381dfc3529aa505b454df64dbbe8c22e931f8
Sunny Patel <sunny2831@hotmail.co.uk>
2018-11-03 14:06:03 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,5 +1,5 @@
 def speak_to_grandma(phrase)
-  if phrase == "I love you grandma!".upcase
+  if phrase == "I LOVE YOU GRANDMA"
     puts "I LOVE YOU TOO PUMPKIN!"
   elsif phrase == phrase.upcase
     puts "NO, NOT SINCE 1938!"
