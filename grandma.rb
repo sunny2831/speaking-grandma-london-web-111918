4f70530d85075ae9a00337d63ef0c49482e9bad4
Sunny Patel <sunny2831@hotmail.co.uk>
2018-11-03 14:16:51 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -4,6 +4,6 @@ def speak_to_grandma(phrase)
   elsif phrase == phrase.upcase
     return "NO, NOT SINCE 1938!"
   else
-    return "HUH!? SPEAK UP, SONNY!"
+    return "HUH?! SPEAK UP, SONNY!"
   end
 end
